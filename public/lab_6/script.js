5ffff89a84e4a93de587abae9fb0ac44713819e9
Gabriel Sestieri <gsestieri15@gmail.com>
2020-10-14 04:34:53 -0400

    lab 6 final w/o bonus

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -34,18 +34,11 @@ document.body.addEventListener('submit', async (evt) => {
     .then((fromServer) => {
       const arr = range(10);
       const randomCountries = arr.map((i) => {
-        list = []
         const listLength = fromServer.length;
         const randomCountryIndex = getRandomInt(listLength);
-        list.append(randomCountryIndex);
-        if (randomCountryIndex in list) {
-          const country = fromServer[randomCountryIndex + 1]
-        } else {
-          const country = fromServer[randomCountryIndex];
-        }
+        const country = fromServer[randomCountryIndex];
         return country;
       });
-      console.table(randomCountries)
 
       const sortedCountries = randomCountries.sort((a, b) => sortFunction(b, a, 'name'));
 
