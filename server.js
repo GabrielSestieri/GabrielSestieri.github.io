bccc3a150a4366faa29b1a5d8bfb11bfdd1756e4
Gabriel Sestieri <gsestieri15@gmail.com>
2020-10-26 20:27:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    res.json(locations);
+    res.json(countries);
   });
 
 app.listen(port, () => {
