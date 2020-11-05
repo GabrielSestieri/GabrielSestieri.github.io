170acdba195735c6d953ccd93d0f87090ef40cb0
Gabriel Sestieri <gsestieri15@gmail.com>
2020-11-05 02:09:06 -0500

    Lab 7 v1
    version1

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -7,13 +7,23 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
   CanvasJS.addColorSet('customColorSet1', [
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    '#4661EE',
+    '#EC5657',
+    '#1BCDD1',
+    '#8FAABB',
+    '#B08BEB',
+    '#3EA0DD',
+    '#F5A52A',
+    '#23BFAA',
+    '#FAA586',
+    '#EB8CC6'
   ]);
 
   return {
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'Change This Title'
+      text: 'Where Should We Eat?'
     },
     axisX: {
       interval: 1,
@@ -22,9 +32,28 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
       gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      title: 'Restaurants By Category',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {customBreaks: [
+        {
+          startValue: 40,
+          endValue: 50,
+          color:'orange',
+          type:'zigzag'
+        },
+        {
+          startValue: 60,
+          endValue: 70,
+          color:'orange',
+          type:'zigzag'
+        },
+        {
+          startValue: 80,
+          endValue: 90,
+          color:'orange',
+          type:'zigzag'
+        }
+      ]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
